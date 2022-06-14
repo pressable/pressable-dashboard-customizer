3a8d9b89506e686bfe07ce95dda3d169323f620f
Nox Dineen <noxdineen@gmail.com>
2022-06-14 09:26:05 -0700

    Update whitelabel-dashboard.php
    Updated formatting and line spacing to match Pressable plugin conventions.

--- whitelabel-dashboard.php
+++ whitelabel-dashboard.php
@@ -1,35 +1,28 @@
 <?php
-
 /*
-Plugin Name: Whitelabel Dashboard
-
+Plugin Name: White-label Dashboard
+Plugin URI: https://pressable.com
 Description: This plugin removes the web host's banner from the WordPress admin dashboard
-
 Version: 1.0.0
-
-Author: Pressable.com
-
-License: GPLv2 or later 
+Author: Pressable
+Author URI: https://pressable.com
+License: GPLv2 
 */
 
 // disable direct file access
-if (!defined('ABSPATH'))
-{
+if (!defined('ABSPATH')) {
     exit;
 }
 
 // plugin can only work for site hosted on Pressable.
-if (!defined('IS_PRESSABLE'))
-    {
-        return;
-    }
+if (!defined('IS_PRESSABLE')) {
+    return;
+}
 
 // remove the "Welcome to Pressable" widget box from dashboard when it is loaded
-function remove_pressable_widget() 
-	{
+function remove_pressable_widget() {
 		remove_meta_box( 'pressable_dashboard_widget', 'dashboard', 'normal' );
-	}
+    }
 
 add_action('wp_dashboard_setup', 'remove_pressable_widget' );
-
 ?>
