bbb7ca31613ab5f1faeb5e88108aef62706707d8
Nox Dineen <noxdineen@gmail.com>
2022-06-20 13:10:37 -0700

    Update whitelabel-dashboard.php
    - Increment versioning
    
    - Add a check to see if functions.php is also going to try removing the widget, only run plugin if that is not the case

--- whitelabel-dashboard.php
+++ whitelabel-dashboard.php
@@ -3,7 +3,7 @@
 Plugin Name: White-label Dashboard
 Plugin URI: https://pressable.com
 Description: This plugin removes the web host's banner from the WordPress admin dashboard
-Version: 1.0.0
+Version: 1.0.1
 Author: Pressable
 Author URI: https://pressable.com
 License: GPLv2 
@@ -19,10 +19,15 @@ if (!defined('IS_PRESSABLE')) {
     return;
 }
 
-// remove the "Welcome to Pressable" widget box from dashboard when it is loaded
-function remove_pressable_widget() {
-		remove_meta_box( 'pressable_dashboard_widget', 'dashboard', 'normal' );
-    }
+// remove the "Welcome to Pressable" widget box from dashboard
+function remove_pressable_widget_with_plugin() {  // must be different name than identical function used in functions.php so function_exists() isn't accidentally triggered
+        remove_meta_box( 'pressable_dashboard_widget', 'dashboard', 'normal' );
+}
 
-add_action('wp_dashboard_setup', 'remove_pressable_widget' );
+// if pressable widget is active, remove it
+function remove_widget_if_active() {
+        if (!function_exists('remove_pressable_widget')) {
+            add_action('wp_dashboard_setup', 'remove_pressable_widget_with_plugin' );
+    }
+}
 ?>
