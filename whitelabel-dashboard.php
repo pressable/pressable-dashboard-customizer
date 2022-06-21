34d901a926886222de4be03257dc1ae4235226c8
Nox Dineen <noxdineen@gmail.com>
2022-06-21 14:53:38 -0700

    Update whitelabel-dashboard.php
    Iterated version number.
    
    Simplified plugin by checking for function name in functions.php within the function called by the action hook instead of having it call a second, internal function.

--- whitelabel-dashboard.php
+++ whitelabel-dashboard.php
@@ -8,28 +8,31 @@ Author: Pressable
 Author URI: https://pressable.com
 License: GPLv2 
 */
-
+ 
 // disable direct file access
 if (!defined('ABSPATH')) {
     exit;
 }
-
+ 
 // plugin can only work for site hosted on Pressable.
 if (!defined('IS_PRESSABLE')) {
     return;
 }
 
-// remove the "Welcome to Pressable" widget box from dashboard
-function remove_pressable_widget_with_plugin() {  // must be different name than identical function used in functions.php so function_exists() isn't accidentally triggered
+// get some info about active theme and its functions.php
+$active_theme_slug = get_stylesheet(); // get active theme's slug as a string
+$theme_functions = file_get_contents(WP_CONTENT_DIR . '/themes/' . $active_theme_slug . '/functions.php'); // get contents active theme's functions.php as a string
+ 
+// what are we looking for in the theme's functions.php?
+$function_name = 'remove_pressable_widget';
+ 
+// remove the "Welcome to Pressable" widget box from dashboard if functions.php isn't already removing it
+function remove_pressable_widget_with_plugin() {
+    if (!strpos($theme_functions, $function_name)) { 
         remove_meta_box( 'pressable_dashboard_widget', 'dashboard', 'normal' );
-}
-
-// if pressable widget is active, remove it
-function remove_widget_if_active() {
-        if (!function_exists('remove_pressable_widget')) {
-            add_action('wp_dashboard_setup', 'remove_pressable_widget_with_plugin' );
     }
 }
 
 add_action('wp_dashboard_setup', 'remove_pressable_widget_with_plugin');
+
 ?>
